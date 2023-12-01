be4d3d429f719c878b51e10b9ea19285a5801f6b
robinpokorny <robin.pokorny@productboard.com>
2023-12-01 15:44:08 +0100

    :zap: Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,67 @@
+private fun parse(input: List<String>) = input
+
+private val substitutions = listOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+)
+
+private fun part1(input: List<String>): Int = input
+    .sumOf { line -> line
+        .mapNotNull { it.toString().toIntOrNull() }
+        .let { it.first() * 10 + it.last() }
+    }
+
+private fun findFirstDigit(line: String): Int = line
+    .findAnyOf(substitutions.flatMap { it.toList() }, 0, true)
+    ?.second!!
+    .let { substitutions.fold(it) { acc, (old, new) -> acc.replace(old, new, true) } }
+    .toInt()
+
+private fun findLastDigit(line: String): Int = line
+    .findLastAnyOf(substitutions.flatMap { it.toList() }, ignoreCase = true)
+    ?.second!!
+    .let { substitutions.fold(it) { acc, (old, new) -> acc.replace(old, new, true) } }
+    .toInt()
+
+private fun part2(input: List<String>): Int = input
+    .sumOf { findFirstDigit(it) * 10 + findLastDigit(it) }
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    println(findFirstDigit("xtwone3four"))
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = parse(rawTestInput)
+    val testInput2 = parse(rawTestInput2)
+    val input = parse(readDayInput(1))
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // PART 1
+    assertEquals(part1(testInput), 142)
+    println("Part1: ${part1(input)}")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // PART 2
+    assertEquals(part2(testInput2), 281)
+    println("Part2: ${part2(input)}")
 }
+
+private val rawTestInput = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+""".trimIndent().lines()
+
+private val rawTestInput2 = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+""".trimIndent().lines()
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,21 +1,17 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readDayInput(day: Int): List<String> {
+    val fileName = day.toString().padStart(2, '0')
+    return Path("src/Day$fileName.txt").readLines()
+}
 
 /**
- * Converts string to md5 hash.
+ * Compares the two inputs and throws if they are not equal
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+fun <T> assertEquals(actual: T, expected: T) {
+    check(actual == expected) { "Assert failed: expected `$expected`, received `$actual`" }
+}
