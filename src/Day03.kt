a339c4e368b19ceb5734910e76ec1c56fd29af71
robinpokorny <robin.pokorny@productboard.com>
2023-12-03 23:15:24 +0100

    :zap: Day 3 part 2 with comments

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,19 +2,21 @@ typealias Schematic = Map<Point, List<Int>>
 
 private fun parse(input: List<String>) = input
 
-
 private fun toSchematic(input: List<String>): Schematic = input
     .flatMapIndexed { i, line ->
         Regex("\\d+")
+            // Find all numbers in the line
             .findAll(line)
             .flatMap { match ->
                 match
                     .range
+                    // Get all points around the number
                     .flatMap { position ->
                         val current = Point(i, position)
                         Point.AROUND.map { current + it }
                     }
                     .toSet()
+                    // Filter out non-symbol points
                     .filter {
                         input
                             .getOrNull(it.x)
@@ -25,10 +27,13 @@ private fun toSchematic(input: List<String>): Schematic = input
                     .map { it to match.value.toInt() }
             }
     }
+    // For each symbol, list all numbers around it
     .groupBy({ it.first }, { it.second })
 
 private fun part1(input: List<String>): Int = toSchematic(input)
-    .flatMap { it.value }.sum()
+    .values
+    .flatten()
+    .sum()
 
 private fun part2(input: List<String>): Int = toSchematic(input)
     .filter { (point, values) -> input[point.x][point.y] == '*' && values.size == 2 }
