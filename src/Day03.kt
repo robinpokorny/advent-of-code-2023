44c142d25a35aac1cae164097ce561bae4a11b0a
robinpokorny <robin.pokorny@productboard.com>
2023-12-03 23:02:37 +0100

    :zap: Day 3 part 2 refactored again

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,41 +3,37 @@ typealias Schematic = Map<Point, List<Int>>
 private fun parse(input: List<String>) = input
 
 
-private fun toSchematic(input: List<String>): Schematic {
-    val symbols = input
-        .map { it.toList() }
-        .flatMapIndexed { i, line ->
-            line.mapIndexedNotNull { j, char ->
-                if (!char.isDigit() && char != '.') Point(i, j) else null
-            }
-        }
-        .toSet()
-
-    return input.flatMapIndexed { i, line ->
+private fun toSchematic(input: List<String>): Schematic = input
+    .flatMapIndexed { i, line ->
         Regex("\\d+")
             .findAll(line)
             .flatMap { match ->
                 match
                     .range
-                    .flatMap {
-                        val current = Point(i, it)
+                    .flatMap { position ->
+                        val current = Point(i, position)
                         Point.AROUND.map { current + it }
                     }
-                    .intersect(symbols)
+                    .toSet()
+                    .filter {
+                        input
+                            .getOrNull(it.x)
+                            ?.getOrNull(it.y)
+                            ?.let { char -> !char.isDigit() && char != '.' }
+                            ?: false
+                    }
                     .map { it to match.value.toInt() }
             }
-    }.groupBy({ it.first }, { it.second })
-}
+    }
+    .groupBy({ it.first }, { it.second })
 
 private fun part1(input: List<String>): Int = toSchematic(input)
     .flatMap { it.value }.sum()
-
 
 private fun part2(input: List<String>): Int = toSchematic(input)
     .filter { (point, values) -> input[point.x][point.y] == '*' && values.size == 2 }
     .values
     .sumOf { (a, b) -> a * b }
-
 
 fun main() {
     val testInput = parse(rawTestInput)
