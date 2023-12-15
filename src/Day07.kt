074c1725b63083abe491d40015fbd2a63a9e804a
robinpokorny <robin.pokorny@productboard.com>
2023-12-15 17:04:48 +0100

    :zap: Day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,4 +1,7 @@
-val labels = "23456789TJQKA".toList()
+val cardStrength = "23456789TJQKA".toList().reversed()
+
+val Joker = cardStrength.indexOf('J')
+val weakJoker = 100 // higher than any other card
 
 enum class Kind() {
     FIVE_OF_A_KIND,
@@ -10,7 +13,7 @@ enum class Kind() {
     HIGH_CARD
 }
 
-data class Hand(
+private data class Hand(
     val cards: List<Int>,
     val bid: Int,
     val kind: Kind = Kind.HIGH_CARD
@@ -20,33 +23,64 @@ private fun parse(input: List<String>) = input
     .map { line ->
         val (cards, bid) = line.split(" ")
 
-        Hand(cards.toList().map { -labels.indexOf(it) }, bid.toInt())
+        Hand(cards.map { cardStrength.indexOf(it) }, bid.toInt())
     }
+
+private fun getKind(labels: Collection<Int>) = when {
+    labels.contains(5) -> Kind.FIVE_OF_A_KIND
+    labels.contains(4) -> Kind.FOUR_OF_A_KIND
+    labels.contains(3) && labels.contains(2) -> Kind.FULL_HOUSE
+    labels.contains(3) -> Kind.THREE_OF_A_KIND
+    labels.count { it == 2 } == 2 -> Kind.TWO_PAIRS
+    labels.contains(2) -> Kind.ONE_PAIR
+    else -> Kind.HIGH_CARD
+}
+
+private val handComparator = compareBy<Hand>(
+    { it.kind },
+    { it.cards[0] },
+    { it.cards[1] },
+    { it.cards[2] },
+    { it.cards[3] },
+    { it.cards[4] })
 
 private fun part1(input: List<Hand>): Int = input
     .map { hand ->
         val labels = hand.cards.groupingBy { it }.eachCount().values
 
-        val kind = when {
-            labels.contains(5) -> Kind.FIVE_OF_A_KIND
-            labels.contains(4) -> Kind.FOUR_OF_A_KIND
-            labels.contains(3) && labels.contains(2) -> Kind.FULL_HOUSE
-            labels.contains(3) -> Kind.THREE_OF_A_KIND
-            labels.count { it == 2 } == 2 -> Kind.TWO_PAIRS
-            labels.contains(2) -> Kind.ONE_PAIR
-            else -> Kind.HIGH_CARD
-        }
+        val kind = getKind(labels)
 
         hand.copy(kind = kind)
     }
-    .sortedWith(compareBy({ it.kind }, { it.cards[0] }, { it.cards[1] }, { it.cards[2] }, { it.cards[3] }, { it.cards[4] }))
+    .sortedWith(handComparator)
     .reversed()
     .mapIndexed { index, hand -> (index + 1) * hand.bid }
     .sum()
 
-private fun part2(input: List<Hand>): Int {
-    return 0
-}
+private fun part2(input: List<Hand>): Int = input
+    .map { hand ->
+        hand
+            // Make Jokers the weakest card
+            .copy(cards = hand.cards.map { if (it == Joker) weakJoker else it })
+    }
+    .map { hand ->
+        val groups = hand.cards.groupingBy { it }.eachCount()
+
+        val jokers = groups[weakJoker] ?: 0
+
+        val labelsWithoutJokers = (groups - (weakJoker)).values.sortedDescending()
+
+        // Add count the jokers as the most common card
+        val updatedCards = listOf((labelsWithoutJokers.firstOrNull() ?: 0) + jokers) + labelsWithoutJokers.drop(1)
+
+        val kind = getKind(updatedCards)
+
+        hand.copy(kind = kind)
+    }
+    .sortedWith(handComparator)
+    .reversed()
+    .mapIndexed { index, hand -> (index + 1) * hand.bid }
+    .sum()
 
 fun main() {
     val testInput = parse(rawTestInput)
@@ -57,7 +91,7 @@ fun main() {
     println("Part1: ${part1(input)}")
 
     // PART 2
-    assertEquals(part2(testInput), 0)
+    assertEquals(part2(testInput), 5905)
     println("Part2: ${part2(input)}")
 }
 
